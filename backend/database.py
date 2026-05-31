9765282399599b09f03bb5e081b1ea4d5dffda16
Pavan Kumar Verma <vermapavan9078@gmail.com>
2026-05-31 12:42:20 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,21 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-DATABASE_URL = []
+DATABASE_URL = "sqlite:///./lms.db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
-# ✅ THIS WAS MISSING
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
